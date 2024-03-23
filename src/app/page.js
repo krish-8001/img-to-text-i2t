1cfc0a1da534b6092146f107ff6a31b1ff2ba765
krish <krish>
2024-03-23 18:12:33 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,16 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Features from "./components/Features";
 import Pricing from "./components/Pricing";
-import UserInfo from "./components/UserInfo";
 
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      {/* <UserInfo/> */}
       <Imagetotext />
       <Pricing/>
       <Features/>
       <Footer />
-      {/* <UserInfo/> */}
     </>
 
   );
