9341aace1e71f3f37dd7e5980c122ee3a2c7a6cf
krish <krish>
2024-04-07 10:31:52 +0530

    icon updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,15 +6,15 @@ import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "image to text converter ",
-  description: "image to text converter ",
+  title: "Image To Text Converter",
+  description: "Convert images to text",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
          <head>
-        <link rel="shortcut icon" type="image/png" href="../../public/text-to-speech.png" />
+        <link rel="shortcut icon" type="image/png" href="https://image-to-text-delta.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimaging.eb7d2204.png&w=64&q=75" />
       </head>
       <body className={inter.className}>
         <NextAuthProvider>
